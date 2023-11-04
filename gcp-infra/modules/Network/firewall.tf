d77ec2b01ae841278a630bc21fdf3cd49a24b439
amira <amira@amira-Lenovo-IdeaPad-Z510>
2023-11-04 05:55:31 +0200

    editing

---
+++ gcp-infra/modules/Network/firewall.tf
@@ -0,0 +1,14 @@
+/*resource "google_compute_firewall" "firewall-ssh" {
+
+  name    = "firewall-ssh"
+  network = google_compute_network.myvpc.name
+  direction = "INGRESS"
+  target_tags = ["private-vm"]
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"] //35.235.240.0/20
+ 
+}*/
